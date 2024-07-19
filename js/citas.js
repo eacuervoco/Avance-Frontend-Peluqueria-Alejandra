096c3170a98a6e78133e9c19147e8febf8c66b89
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-07-19 01:57:51 -0500

    refactor: se optimiza el método de inicialización de Sidenav y la función para activar el contraste alto.

--- js/citas.js
+++ js/citas.js
@@ -1,36 +1,24 @@
 // Inicialización del plugin para ocultar nav en pantallas medianas y pequeñas
 document.addEventListener('DOMContentLoaded', function () {
-  var elem = document.querySelector('.sidenav');
-  var instance = M.Sidenav.init(elem);
+  const sidenav = document.querySelector('.sidenav');
+  M.Sidenav.init(sidenav);
 });
 
 // ACTIVAR CONTRASTE ALTO
-// Seleccionar los dos inputs que están dentro de los elementos con la clase switch
-const inputs = document.querySelectorAll(".switch input[type=checkbox]");
+function toggleHighContrast() {
+  const elements = document.querySelectorAll("html *");
+  const inputs = document.querySelectorAll(".switch input[type=checkbox]");
 
-// let html = document.querySelector("html");
-let elements = document.querySelectorAll("html *");
-
-for (let i = 0; i < inputs.length; i++) {
-  // Capturar el evento de tipo change de los dos inputs
-  inputs[i].addEventListener("change", function () {
-    /// Obtener el valor de la propiedad checked que puede ser true or false
-    const isChecked = inputs[i].checked;
-
-    if (isChecked) {
-      // html.classList.add("high-contrast")
-      for (var j = 0; j < elements.length; j++) {
-        elements[j].classList.add("high-contrast");
-      }
-    } else {
-      // Si el checkbox no está marcado, quitar la clase .high-contrast a cada elemento
-      // html.classList.remove("high-contrast")
-      for (var j = 0; j < elements.length; j++) {
-        elements[j].classList.remove("high-contrast");
-      }
-    }
+  inputs.forEach(input => {
+    input.addEventListener("change", function () {
+      const isChecked = this.checked;
+      elements.forEach(element => element.classList.toggle("high-contrast", isChecked));
+    });
   });
 }
+document.addEventListener('DOMContentLoaded', toggleHighContrast);
+
+
 //inicio de codigo para cada modulo.
 
 const myForm = document.querySelector('form');

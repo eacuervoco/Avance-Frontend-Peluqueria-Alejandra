957dc39410955c606d0a0d0843abcccd74984fa8
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-07-21 18:02:37 -0500

    feat: se agrega función para hacer una petición GET al cargar la pagina y mostrar así los horarios ya registrados en la base de datos.

--- js/horarios.js
+++ js/horarios.js
@@ -20,6 +20,61 @@ inputs.forEach(input => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchWorkingHours();
+});
+
+async function fetchWorkingHours() {
+    try {
+        const token = sessionStorage.getItem('accessToken');
+        const response = await fetch("http://localhost:6543/api/v1/workinghours", {
+            method: "GET",
+            headers: { 'Authorization': `Bearer ${token}` }
+        });
+        if (!response.ok) {
+            throw new Error('La petición GET falló');
+        }
+        const data = await response.json();
+
+        displayWorkingHours(data.content);
+    } catch (error) {
+        console.error(error.message);
+        alert(error);
+    }
+}
+
+function displayWorkingHours(data) {
+    const tbody = document.querySelector('tbody');
+    // Asegurarse de que data siempre sea un array para manejarlo de forma uniforme
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+
+    data.forEach(item => {
+        const { startDate, endDate } = item;
+        const initialDate = startDate.split("T")[0];
+        const initialTime = startDate.split("T")[1];
+        const finalDate = endDate.split("T")[0];
+        const finalTime = endDate.split("T")[1];
+
+        const newRow = document.createElement('tr');
+
+        const weekDays = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+        const startDateObj = new Date(initialDate);
+        const dayOfWeek = weekDays[startDateObj.getDay()];
+
+        newRow.innerHTML = `
+            <td>${dayOfWeek}</td>
+            <td>${initialDate}</td>
+            <td>${convertTo12Hour(initialTime)}</td>
+            <td>${finalDate}</td>
+            <td>${convertTo12Hour(finalTime)}</td>
+        `;
+
+        tbody.appendChild(newRow);
+    });
+}
+
 const form = document.querySelector('form');
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -54,42 +109,7 @@ form.addEventListener('submit', async (event) => {
 
         const data = await response.json();
 
-        const { startDate, endDate } = data;
-        const initialDate = startDate.split("T")[0];
-        const initialTime = startDate.split("T")[1];
-        const finalDate = endDate.split("T")[0];
-        const finalTime = endDate.split("T")[1];
-
-        const tbody = document.querySelector('tbody');
-        const newRow = document.createElement('tr');
-        const weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-        const startDateObj = new Date(initialDate);
-        const dayOfWeek = weekDays[startDateObj.getDay()];
-
-        newRow.innerHTML = `
-            <td>${dayOfWeek}</td>
-            <td>${initialDate}</td>
-            <td>${formatTime(initialTime)}</td>
-            <td>${finalDate}</td>
-            <td>${formatTime(finalTime)}</td>
-        `;
-
-        function formatTime(time) {
-            const [hours, minutes] = time.split(':');
-            let period = 'AM';
-            let formattedHours = parseInt(hours);
-
-            if (formattedHours >= 12) {
-                period = 'PM';
-                if (formattedHours > 12) {
-                    formattedHours -= 12;
-                }
-            }
-
-            return `${formattedHours.toString().padStart(2, '0')}:${minutes} ${period}`;
-        }
-
-        tbody.appendChild(newRow);
+        displayWorkingHours(data);
     } catch (error) {
         console.error('Error capturado: ', error.message);
         alert("Error: " + JSON.stringify(error.message));
@@ -102,21 +122,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const elements = document.querySelectorAll('.datepicker');
 
     const options = {
-      autoClose: true,
-      format: 'yyyy-mm-dd'
+        autoClose: true,
+        format: 'yyyy-mm-dd'
     };
     const instances = M.Datepicker.init(elements, options);
-  });
+});
 
   // CONFIGURACIÓN PARA EL INPUT DE TIME
 document.addEventListener('DOMContentLoaded', function () {
     const elements = document.querySelectorAll('.timepicker');
     const options = {
-      defaultTime: '08:00', 
-      format: 'HH:mm'
+        defaultTime: '08:00',
+        format: 'HH:mm'
     };
     const instances = M.Timepicker.init(elements, options);
-  });
+});
 
 // Función para convertir hora de formato 12 horas a formato 24 horas
 function convertTo24Hour(time) {
@@ -132,4 +152,19 @@ function convertTo24Hour(time) {
     }
 
     return `${hours}:${minutes}`;
+}
+
+function convertTo12Hour(time) {
+    const [hours, minutes] = time.split(':');
+    let period = 'AM';
+    let formattedHours = parseInt(hours);
+
+    if (formattedHours >= 12) {
+        period = 'PM';
+        if (formattedHours > 12) {
+            formattedHours -= 12;
+        }
+    }
+
+    return `${formattedHours.toString().padStart(2, '0')}:${minutes} ${period}`;
 }

df213f371b8cfeec3694b25fcb7271d9225a216c
DairoCamacho <74800666+DairoCamacho@users.noreply.github.com>
2024-07-21 02:33:42 -0500

    feat: se crea el modulo para que los empleados registren los horarios de trabajo

---
+++ js/horarios.js
@@ -0,0 +1,119 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elem = document.querySelector('.sidenav');
+    M.Sidenav.init(elem);
+});
+
+const inputs = document.querySelectorAll(".switch input[type=checkbox]");
+const elements = document.querySelectorAll("html *");
+
+inputs.forEach(input => {
+    input.addEventListener("change", function () {
+        const isChecked = input.checked;
+
+        elements.forEach(element => {
+            if (isChecked) {
+                element.classList.add("high-contrast");
+            } else {
+                element.classList.remove("high-contrast");
+            }
+        });
+    });
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const initialDate = document.getElementById('initialDate').value;
+    const initialTime = document.getElementById('initialTime').value;
+    const finalDate = document.getElementById('finalDate').value;
+    const finalTime = document.getElementById('finalTime').value;
+
+    const jsonData = {
+        startDate: `${initialDate}T${initialTime}`,
+        endDate: `${finalDate}T${finalTime}`,
+        employeeId: 2
+    };
+
+    try {
+        const token = sessionStorage.getItem('accessToken');
+
+        const response = await fetch("http://localhost:6543/api/v1/workinghours", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(jsonData)
+        });
+
+        if (!response.ok) {
+            const errorBody = await response.text();
+            throw new Error(errorBody);
+        }
+
+        const data = await response.json();
+
+        const { startDate, endDate } = data;
+        const initialDate = startDate.split("T")[0];
+        const initialTime = startDate.split("T")[1];
+        const finalDate = endDate.split("T")[0];
+        const finalTime = endDate.split("T")[1];
+
+        const tbody = document.querySelector('tbody');
+        const newRow = document.createElement('tr');
+        const weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        const startDateObj = new Date(initialDate);
+        const dayOfWeek = weekDays[startDateObj.getDay()];
+
+        newRow.innerHTML = `
+            <td>${dayOfWeek}</td>
+            <td>${initialDate}</td>
+            <td>${formatTime(initialTime)}</td>
+            <td>${finalDate}</td>
+            <td>${formatTime(finalTime)}</td>
+        `;
+
+        function formatTime(time) {
+            const [hours, minutes] = time.split(':');
+            let period = 'AM';
+            let formattedHours = parseInt(hours);
+
+            if (formattedHours >= 12) {
+                period = 'PM';
+                if (formattedHours > 12) {
+                    formattedHours -= 12;
+                }
+            }
+
+            return `${formattedHours.toString().padStart(2, '0')}:${minutes} ${period}`;
+        }
+
+        tbody.appendChild(newRow);
+    } catch (error) {
+        console.error('Error capturado: ', error.message);
+        alert("Error: " + JSON.stringify(error.message));
+    }
+})
+
+
+// CONFIGURACIÓN PARA EL INPUT DE DATE
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.datepicker');
+
+    const options = {
+      autoClose: true,
+      format: 'yyyy-mm-dd'
+    };
+    const instances = M.Datepicker.init(elements, options);
+  });
+
+  // CONFIGURACIÓN PARA EL INPUT DE TIME
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.timepicker');
+    const options = {
+      defaultTime: '08:00', 
+      format: 'HH:mm'
+    };
+    const instances = M.Timepicker.init(elements, options);
+  });
